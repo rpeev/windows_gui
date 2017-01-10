5d4948b5c91ae1f3d2f4ce4f23b0deda2d11fb35
Radoslav Peev <rpeev@ymail.com>
2017-01-10 13:14:05 +0200

    Rename and tweak test file

--- test/ffi-wingui-core_test.rb
+++ test/ffi-wingui-core_test.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 
 require_relative '../lib/ffi-wingui-core/common'
+require 'minitest'
 require 'minitest/autorun'
 
-class FooTest < Minitest::Test
+class WinGUITest < Minitest::Test
   def test_version
     refute_nil WinGUI::VERSION
   end
